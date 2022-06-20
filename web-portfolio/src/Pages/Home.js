2fde9c38ca517cf62408b980ff9079632fc75986
Alex Roman <aroman27@ucmerced.edu>
2022-06-19 18:14:40 -0700

    Added home page

---
+++ web-portfolio/src/Pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Home = () => {
+
+    return (
+        <>
+            <h1 className='text-white'> hello world </h1>
+        </>
+    );
+}
+
+
+export default Home;
